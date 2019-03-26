d655952fd21d269f83c54c756225fa3178d847de
rajadesaboina <raja.desaboina@gmail.com>
2019-03-26 12:58:56 +0530

    Added New File

---
+++ Devops/src/Sample_Java/Sample.java
@@ -0,0 +1,11 @@
+package Sample_Java;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		/* TODO Auto-generated method stub */
+         System.out.println("Automation");
+	}
+
+}
